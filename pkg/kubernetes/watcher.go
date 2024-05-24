f4d62f69fc143c36cf625a6cf8811da856357175
Ilya Gavrilov <gilyav@gmail.com>
2024-05-24 20:52:38 +0200

    revert "Don't call updateCurrentlyTargetedPods method unnecessarily" (#60)

--- pkg/kubernetes/watcher.go
+++ pkg/kubernetes/watcher.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"regexp"
-	"strings"
 
 	"github.com/rs/zerolog/log"
 	v1 "k8s.io/api/core/v1"
@@ -64,20 +63,7 @@ func (watcher *Watcher) watchKubesharkConfigMap(ctx context.Context) error {
 				continue
 			}
 
-			var oldRegex string
-			var oldNamespaces []string
-			if watcher.regex != nil {
-				oldRegex = watcher.regex.String()
-			}
-			oldNamespaces = watcher.namespaces
 			watcher.regex, watcher.namespaces = SyncConfig(event.Object.(*v1.ConfigMap))
-
-			// No change in pod regex or targeted namespaces
-			if watcher.regex != nil {
-				if watcher.regex.String() == oldRegex && strings.Join(watcher.namespaces, ",") == strings.Join(oldNamespaces, ",") {
-					continue
-				}
-			}
 
 			err = updateCurrentlyTargetedPods(ctx, watcher.clientSet, watcher.regex, watcher.namespaces, watcher.callback)
 			if err != nil {
