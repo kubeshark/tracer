caea5a6ceccca14512e802b0aa5a3a246f3ffe87
bogdan.balan1 <bogdanvalentin.balan@1nce.com>
2025-11-09 09:27:56 +0200

    Fix verifier

--- bpf/include/cgroups.h
+++ bpf/include/cgroups.h
@@ -7,7 +7,7 @@ Copyright (C) Kubeshark
 
 #include "maps.h"
 
-int should_target_cgroup(__u64 cgroup_id) {
+static __always_inline int should_target_cgroup(__u64 cgroup_id) {
     __u32 zero = 0;
     struct configuration* s = bpf_map_lookup_elem(&settings, &zero);
     if (s && (s->flags & CONFIGURATION_PASS_ALL_CGROUPS)) {
